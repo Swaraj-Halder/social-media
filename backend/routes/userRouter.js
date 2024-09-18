c7b6e26560f756758830e69db99e0c15ef1948ef
Swaraj-Halder <154491830+Swaraj-Halder@users.noreply.github.com>
2024-09-19 00:16:53 +0530

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,13 @@
 const express = require("express")
-const {loginUser, signupUser, changePassword, deleteUser} = require("../controllers/userController")
+const {loginUser, getOtpForSignup, signupUser, changePassword, deleteUser} = require("../controllers/userController")
 const router = express.Router()
 
 // Login route
 router.post("/login", loginUser)
+
+// Get otp for varify email to singup
+
+router.post("/get-otp-for-signup", getOtpForSignup)
 
 // Signup route
 router.post("/signup", signupUser)
